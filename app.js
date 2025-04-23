be7e05b69743047c54a0a87bd06d6644fd9df038
maytega <maytega@uniovi.es>
2025-04-23 12:53:16 +0200

    response /

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
 const app = express()
 
+app.get("/", (req, res) => {res.send("<h1> Pagina inicial </h1>")})
 app.listen(8081, () => {console.log("Servidor activo en 8081")})
